d6b9c58a173c1efcf9bea5fb48a42dfa020a3460
Jonatan <barakawins@gmail.com>
2025-04-04 20:38:30 -0300

    properly await for tasks finalization.

--- async-tokio/src/main.rs
+++ async-tokio/src/main.rs
@@ -15,14 +15,15 @@ async fn main() {
     one().await;
     two().await;
     sleep(Duration::from_secs(1)).await;
+    let mut handles = vec!();
     let mut i = 0;
-    while i <= 20 {
-        spawn(one());
-        spawn(two());
+    while i <= 600 {
+        handles.push(spawn(one()));
+        handles.push(spawn(two()));
         i += 1;
     };
-    // since spawn just runs the the tasks concurrently we gotta get them some
-    // time them to finish. If we omit the following line the program finishes
-    // inmediatly without waitting for onw() or two().
-    sleep(Duration::from_millis(300)).await;
+    // now we await for all the concurrent tasks to finish.
+    for handle in handles {
+        let _ = handle.await;
+    }
 }
